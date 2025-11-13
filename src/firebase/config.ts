60e6037c55eaaaa038c02187a1c94039641ed5a2
Ice Wine <icewine1122@gmail.com>
2025-11-13 15:00:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9246504063-6757a",
+  "appId": "1:1045689485762:web:b07d0395c4c655dba1ae5b",
+  "apiKey": "AIzaSyAKm-JQPqSW0-bcULeVQfkuZ6RlZ61RCWU",
+  "authDomain": "studio-9246504063-6757a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1045689485762"
+};
